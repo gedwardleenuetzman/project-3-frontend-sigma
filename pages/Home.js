f1b37840804f4fc121640e2f437a3e8fcf649f07
Garrison Nuetzmann <gedwardleenuetzmann@gmail.com>
2023-04-30 15:10:55 -0500

    Translate works on Home, persists thru page swap.

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,5 @@
 /*global google*/
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Container, Typography, Button } from '@mui/material';
 import { useSession, signIn, signOut, getSession } from "next-auth/react"
 import AccountMenu from '../src/AccountMenu';
@@ -12,9 +12,24 @@ export default function Home() {
     // will have sign in button or avatar button with drop down to navigate
 
     // avatar with dropdown will be on all pages, dropdown menu will have Home, Order, and Manage options
+
+    useEffect(() => {
+        // Load the Google Translate API script
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+        script.async = true;
+        document.body.appendChild(script);
+    
+        // Define the `googleTranslateElementInit` function
+        window.googleTranslateElementInit = () => {
+          new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+        };
+      }, []);
     
     return (
         <React.Fragment>
+            <div id="google_translate_element"></div>
             <AccountMenu></AccountMenu>
             <RouteDrawer layout={[
                 [{text: "Home", route: "/Home"}],
