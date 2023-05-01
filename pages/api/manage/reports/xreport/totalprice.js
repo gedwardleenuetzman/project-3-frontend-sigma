e95d482d8a911602c43bb0b01a8b846e7c4693e1
Ralph Vicente <ralphvicente@tamu.edu>
2023-04-30 20:53:58 -0500

    Manager Home Solidified

---
+++ pages/api/manage/reports/xreport/totalprice.js
@@ -0,0 +1,15 @@
+import DailyOrders from "sql/models/dailyorders"
+
+export default async function handler(req, res) {
+    if(req.method === "GET"){
+        try {
+            const sum = await DailyOrders.sum('total_price');
+            console.log('The sum of all total_price values is:', sum);
+
+            res.send(sum);
+        } catch(error) {
+            console.error('Error:', error);
+            res.status(500).send(error);
+        }
+    }
+}
