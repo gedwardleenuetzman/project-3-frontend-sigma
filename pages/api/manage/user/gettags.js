692ce6027dd2fcbb78c196531c3ad193423bd15d
prithvi-patel-tamu <prith.vi@tamu.edu>
2023-05-03 19:40:23 -0500

    fixed gettags

--- pages/api/manage/user/gettags.js
+++ pages/api/manage/user/gettags.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         const session = await getServerSession(req, res, authOptions)
 
         if (!session) {
-            return res.status(401).json({ error: 'You must be authenticated to access this resource.' });
+            return res.status(200).json([]);
         }
       
         const user = await Models.Users.findOne({ where: { email: session.user.email } })
